7a282e1fe4bd26a6465b99c11319a4844ced6be1
Chikara Okada <chikara@gmail.com>
2018-03-29 16:00:48 +0900

    Create show actions and show views implemented

--- spec/features/exercises/creating_exercises_spec.rb
+++ spec/features/exercises/creating_exercises_spec.rb
@@ -17,10 +17,10 @@ RSpec.feature "Creating exercises" do
     fill_in "Activity date", with: "2016-07-26"
     click_button "Create Exercise"
 
-    expect(page).to have_content("Excercise has been created")
+    expect(page).to have_content("Exercise has been created")
 
     exercise = Exercise.last
-    expect(current_path).to eq(user_exercises_path(@john, exercise))
+    expect(current_path).to eq(user_exercise_path(@john, exercise))
     expect(exercise.user_id).to eq(@john.id)
   end
 end
