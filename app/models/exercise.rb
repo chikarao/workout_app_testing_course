f41d53f52954548746b9b0c4c9614c4fd4783727
Chikara Okada <chikara@gmail.com>
2018-03-29 16:19:30 +0900

    Invalid data in exercise scenario test and validation

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,6 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+
+  validates :workout, :workout_date, presence: true
+  validates_numericality_of :duration_in_min, only_integer: true
 end
--- spec/features/exercises/creating_exercises_spec.rb
+++ spec/features/exercises/creating_exercises_spec.rb
@@ -23,4 +23,25 @@ RSpec.feature "Creating exercises" do
     expect(current_path).to eq(user_exercise_path(@john, exercise))
     expect(exercise.user_id).to eq(@john.id)
   end
+
+  scenario "with INvalid inputs" do
+    visit "/"
+    click_link "My Lounge"
+    click_link "New Workout"
+    expect(page).to have_link("Back")
+
+    fill_in "Duration", with: "a"
+    fill_in "Workout details", with: ""
+    fill_in "Activity date", with: ""
+    click_button "Create Exercise"
+
+    expect(page).to have_content("Exercise has not been created")
+    expect(page).to have_content("Duration in min is not a number")
+    expect(page).to have_content("Workout can't be blank")
+    expect(page).to have_content("Workout date can't be blank")
+
+    # exercise = Exercise.last
+    # expect(current_path).to eq(user_exercise_path(@john, exercise))
+    # expect(exercise.user_id).to eq(@john.id)
+  end
 end
