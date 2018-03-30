b6fe663c5d3b0c2b69e8c468301ebded6671b4a1
Chikara Okada <chikara@gmail.com>
2018-03-30 11:05:33 +0900

    Validations to user added

--- spec/features/users/signup_users_spec.rb
+++ spec/features/users/signup_users_spec.rb
@@ -12,21 +12,21 @@ RSpec.feature "Sign up Users" do
     click_button "Sign up"
 
     expect(page).to have_content("Welcome! You have signed up successfully.")
-    # expect().to eq 200
-    # expect(path). to eq(path)
     visit "/"
     expect(page).to have_content("John Doe")
   end
-  # context "User does not supply required credentials" do
-  #   visit "/"
-  #   click_link "Sign up"
-  #   fill_in "Email", with: ""
-  #   fill_in "Password", with: ""
-  #   fill_in "Password Confirmation" with: ""
-  #   click_button "Sign up"
-  #
-  #   expect(page).to have_content("You must provide email and password")
-  #   expect().to eq 200
-  #   expect(path). to eq(path)
-  # end
+  scenario "with invalid credentials" do
+   visit "/"
+
+   click_link "Sign up"
+   fill_in "First name", with: ""
+   fill_in "Last name", with: ""
+   fill_in "Email", with: "john@example.com"
+   fill_in "Password",  with: "password"
+   fill_in "Password confirmation",  with: "password"
+   click_button "Sign up"
+
+   expect(page).to have_content("First name can't be blank")
+   expect(page).to have_content("Last name can't be blank")
+ end
 end
